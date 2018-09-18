02f0a7d589c9d233f1d87302e913decd5d36da71
Liam Diprose <ldi30@uclive.ac.nz>
2018-09-18 19:03:52 +1200

    Automate build with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,48 @@
 var gulp = require("gulp");
-
 var sass = require("gulp-sass")
 var mustache = require("gulp-mustache")
+var yaml = require("js-yaml")
+var fs = require("fs")
+var rename = require("gulp-rename")
+var browserSync = require('browser-sync').create();
 
-
-gulp.task('html', function() {
+function html () {
     return gulp.src('src/*.template.html')
-        .pipe(mustache())
-        .pipe(gulp.dest('index.html'))
-});
+        .pipe(mustache(yaml.safeLoad(fs.readFileSync('src/content.yml', 'utf8'))))
+        .pipe(rename("index.html"))
+        // .pipe(gulp.dest('dist/'))
+        .pipe(gulp.dest('.')) // Place index in root directoy for .github.io hosting
+}
 
-gulp.task('main-styles', function() {
+function mainStyles () {
     return gulp.src('src/style.scss')
     .pipe(sass())
-    .pipe(gulp.dest('dist/style.css'))
-});
+    .pipe(gulp.dest('dist/'))
+}
 
-gulp.task('print-styles', function() {
+function printStyles () {
     return gulp.src('src/print.scss')
     .pipe(sass())
-    .pipe(gulp.dest('dist/print.css'))
-});
+    .pipe(gulp.dest('dist/'))
+}
+
+const compile = gulp.parallel([html, mainStyles, printStyles]);
+
+function watch() {
+
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch(['src/print.scss', 'src/style.scss'], gulp.parallel([mainStyles, printStyles]))
+        .on('change', () => browserSync.reload());
+    gulp.watch(['src/content.yml', 'src/cv.template.html'], html)
+        .on('change', () => browserSync.reload());
+}
 
 
-gulp.task('default', ['html', 'main-styles', 'print-styles']);
+
+module.exports.default = compile
+module.exports.watch = watch
